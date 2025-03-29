d71acf3cf82952181c571612370135460ab24d51
mrswapon <mrswapon15@gmail.com>
2025-03-29 11:28:18 +0600

    Onboarding Screen Done

--- lib/views/screen/Splash/splash_screen.dart
+++ lib/views/screen/Splash/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:perfect_catch_dating_app/utils/app_images.dart';
+import '../../../helpers/prefs_helpers.dart';
 import '../../../helpers/route.dart';
+import '../../../utils/app_constants.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,22 +15,21 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
 
 
-  /*
+
   @override
-  void initState() {
+  void initState()  {
     Future.delayed(const Duration(seconds: 3), () {
-      *//*  var isLogged = await PrefsHelper.getBool(AppConstants.isLogged);
+  /*var isLogged = await PrefsHelper.getBool(AppConstants.isLogged);
       if (isLogged == true) {
         Get.offAllNamed(AppRoutes.homeScreen);
       } else {
         Get.offAllNamed(AppRoutes.onboardingScreen);
-      }
-      *//*
-      Get.offAllNamed(AppRoutes.homeScreen);
+      }*/
+      Get.offAllNamed(AppRoutes.onboardingScreen);
     });
     // TODO: implement initState
     super.initState();
-  }*/
+  }
 
   @override
   Widget build(BuildContext context) {
